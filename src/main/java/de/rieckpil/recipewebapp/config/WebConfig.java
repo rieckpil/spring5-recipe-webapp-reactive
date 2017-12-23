d934c244806b926e54b7386a059d7a2fba85e322
Philip Riecks <philip@riecks.io>
2017-12-23 14:56:08 +0100

    add functional webflux endpoint

---
+++ src/main/java/de/rieckpil/recipewebapp/config/WebConfig.java
@@ -0,0 +1,26 @@
+package de.rieckpil.recipewebapp.config;
+
+import de.rieckpil.recipewebapp.domain.Recipe;
+import de.rieckpil.recipewebapp.services.RecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    RouterFunction<?> routes(RecipeService recipeService) {
+        return RouterFunctions.route(GET("/api/recipes"),
+                serverRequest -> ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(recipeService.getRecipes(), Recipe.class));
+    }
+
+}
